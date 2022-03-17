082c914910b59dc16c65c683ddf3e5977878c549
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-03-17 19:27:55 +0530

    Time: 3 ms (36.32%), Space: 40.9 MB (57.70%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -3,17 +3,17 @@ class Solution {
         if (x==0) return 0;
         
         // Binary search
-        long low=0,high=x;
+        int low=0,high=x;
         long mid=0;
         while (low<=high) {
-            mid = (low+high)/2;
-            System.out.println(low+" "+high+" : "+mid);
+            mid = ((long)low+high)/2;
+            //System.out.println(low+" "+high+" : "+mid);
             if (mid*mid==x)
                 return (int)mid;
             else if (mid*mid>x)
-                high=mid-1;
+                high=(int)mid-1;
             else
-                low=mid+1;
+                low=(int)mid+1;
         }
         return (int)high;
     }
