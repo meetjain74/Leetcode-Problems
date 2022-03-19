3bc7524d7f07b96b960d7dc8441958510df8b09d
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-03-19 12:58:06 +0530

    Time: 1 ms (81.74%), Space: 41.5 MB (24.52%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -2,7 +2,7 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int ans = 0;
-        // Integer type in Java is signed and there is no unsigned int. \
+        // Integer type in Java is signed and there is no unsigned int. 
         // So the input 2147483648 is represented in Java as -2147483648 (in java int type has a         // cyclic representation, that means Integer.MAX_VALUE+1==Integer.MIN_VALUE).
         // This force us to use n!=0 in the while condition and we cannot use n>0
         while (n!=0) {
