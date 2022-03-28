c75b451012791d9b9c4870631e55d68394656a4c
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-03-28 22:40:47 +0530

    Time: 1 ms (92.55%), Space: 44.7 MB (46.00%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,26 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> permutations = new ArrayList<>();
-        permute(permutations, new ArrayList<Integer>(), nums, 0);
+        permute(permutations,nums,0);
         return permutations;
     }
     
-    public void permute(List<List<Integer>> permutations, List<Integer> selected, 
-                           int[] nums, int pos) {
-        if(pos == nums.length) {
-            permutations.add(new ArrayList<Integer>(selected));
+    public void permute(List<List<Integer>>permutations, int[] nums,int i) {
+        if (i==nums.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j : nums)
+                temp.add(j);
+            permutations.add(temp);
             return;
         }
         
-        // Iterating over all elements in the array
-        for(int i=0;i<nums.length;i++) {
-            // If element already selected, continue loop
-            if(selected.contains(nums[i])) 
-                continue;
+        for (int j=i;j<nums.length;j++) {
+            // swap nums[i] and nums[j]
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
             
-            selected.add(nums[i]);
-            permute(permutations, selected, nums, pos+1);
-            selected.remove(selected.size()-1); //Remove latest element added
+            permute(permutations,nums,i+1);
+            
+            // Unswap nums[i] and nums[j]
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
         }
     }
 }
