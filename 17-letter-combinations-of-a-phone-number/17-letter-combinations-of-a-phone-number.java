f0e9e269641cfdca71ca8c4efc5de5b4f479ce9d
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-03-29 15:33:05 +0530

    Time: 2 ms (62.69%), Space: 42.5 MB (67.04%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,25 @@
 class Solution {
+    public String[] keys = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        for (int i=0,len=digits.length();i<len;i++) {
-            char ch = digits.charAt(i);
-            result = CartesianProduct(result,mapping(ch));
-        }
-        return result;
+        List<String> ans = new ArrayList<>();
+        if (digits.length()!=0)
+            helper(ans,digits,0,"");
+        return ans;
     }
     
-    // Returns cartesian product of two lists
-    private List<String> CartesianProduct(List<String> list1, List<String> list2) {
-        List<String> product = new ArrayList<>();
-        if (list1.isEmpty()) 
-            return list2;
+    //i is iterator for the string digits
+    private void helper(List<String> ans,String digits,int i,String temp) {
+        if (i==digits.length()) {
+            ans.add(temp);
+            return;
+        }
         
-        for (int i=0;i<list1.size();i++) {
-            for (int j=0;j<list2.size();j++) {
-                product.add(list1.get(i)+list2.get(j));
-            }
-        }
-        return product;
-    }
-    
-    private List<String> mapping(char num) {
-        switch(num) {
-            case '2': return Arrays.asList("a","b","c");
-            case '3': return Arrays.asList("d","e","f");
-            case '4': return Arrays.asList("g","h","i");
-            case '5': return Arrays.asList("j","k","l");
-            case '6': return Arrays.asList("m","n","o");
-            case '7': return Arrays.asList("p","q","r","s");
-            case '8': return Arrays.asList("t","u","v");
-            case '9': return Arrays.asList("w","x","y","z");
-            default: return Arrays.asList("");
+        int digit = digits.charAt(i)-'0';
+        String key = keys[digit];
+        
+        for (int go=0;go<key.length();go++) {
+            helper(ans,digits,i+1,temp+Character.toString(key.charAt(go)));
         }
     }
 }
