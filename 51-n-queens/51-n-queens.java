eebba679c8a3360d7d7f8aef706200ef60e9527c
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-03-30 10:54:36 +0530

    Time: 9 ms (37.20%), Space: 47 MB (15.42%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -5,14 +5,29 @@ class Solution {
         // Chess board - true representing queen exists
         boolean chess[][] = new boolean[n+1][n+1];
         
-        placeQueens(ans,chess,n,1);
+        /*Queen present in that column or not*/
+        boolean col[] = new boolean[n+1];
+
+        /*Queen present in +ve diagonal or not - 
+        diagonal represented by sum of row and col coordinates
+        possible values are from 2(1+1) to 2*n(n+n) */
+        boolean diag45[] = new boolean[2*n+1];
+
+        /*Queen present at negative diagonal or not - 
+        diagonal represented by difference of row and col coordinates
+        possible values are from 1-n to n-1 
+        Shift all values by n-1
+        Array size should be (n-1+n-1+1)=(2n-1) */
+        boolean diag135[] = new boolean[2*n-1];
+        
+        placeQueens(ans,chess,n,1,col,diag45,diag135);
         
         return ans;
     }
     
     // Placing queens row wise
-    public void placeQueens(List<List<String>> ans,boolean[][] chess,
-                            int n,int rowNum) {
+    public void placeQueens(List<List<String>> ans,boolean[][] chess,int n,
+                            int rowNum,boolean[] col,boolean[] diag45,boolean[] diag135) {
         
         // Base case
         if (rowNum>n) {
@@ -22,63 +37,22 @@ class Solution {
         
         // Placing one by one at each column
         for (int i=1;i<=n;i++) {
-            if (!isConflict(chess,rowNum,i,n)) {
+            if (!isConflict(chess,rowNum,i,n,col,diag45,diag135)) {
+                col[i] = diag45[rowNum+i] = diag135[rowNum-i+n-1] = true;
                 chess[rowNum][i]=true;
-                placeQueens(ans,chess,n,rowNum+1);
+                placeQueens(ans,chess,n,rowNum+1,col,diag45,diag135);
                 chess[rowNum][i]=false;
+                col[i] = diag45[rowNum+i] = diag135[rowNum-i+n-1] = false;
             }
         }
     }
     
     // Check whether conflict exists at pos (x,y)
-    public boolean isConflict(boolean[][] chess,int x,int y,int n) {
-        
-        // Check if queen exist in the given row
-        // Not required - as we place only one in a row
-        // for (int i=1;i<=n;i++) {
-        //     if (chess[x][i])
-        //         return true;
-        // }
-        
-        // Check if queen exist in the given column
-        // Upto x only 
-        for (int i=1;i<=x;i++) {
-            if (chess[i][y])
-                return true;
-        }
-        
-        // Check if queen exist in the -ve slope diagonal
-        int row=x-1,col=y-1;
-        while(row>0 && col>0) {
-            if (chess[row--][col--])
-                return true;
-        }
-        
-        // Not required
-        // row=x+1;
-        // col=y+1;
-        // while(row<=n && col<=n) {
-        //     if (chess[row++][col++])
-        //         return true;
-        // }
-        
-        // Check if queen exist in the +ve slope diagonal
-        row=x-1;
-        col=y+1;
-        while(row>0 && col<=n) {
-            if (chess[row--][col++])
-                return true;
-        }
-        
-        // Not required
-        // row=x+1;
-        // col=y-1;
-        // while(row<=n && col>0) {
-        //     if (chess[row++][col--])
-        //         return true;
-        // }
-        
-        return false;
+    public boolean isConflict(boolean[][] chess,int x,int y,int n,
+                             boolean[] col,boolean[] diag45,boolean[] diag135) {
+        if (col[y] || diag45[x+y] || diag135[x-y+n-1])
+            return true;
+        return false;      
     }
     
     public List<String> getStringList(boolean[][] matrix,int n) {
