d6a3de1ceceef146cb72fb9baa0608f864d0cdbd
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-04-03 15:15:11 +0530

    Time: 9 ms (39.48%), Space: 49.7 MB (60.85%) - LeetHub

---
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        
+        List<Integer> tempMax = findElement(nums);
+        System.out.println(tempMax);
+        if (count(nums,tempMax.get(0))>nums.length/3)
+            ans.add(tempMax.get(0));
+        if (tempMax.get(1)!=tempMax.get(0) && count(nums,tempMax.get(1))>nums.length/3)
+            ans.add(tempMax.get(1));
+        
+        return ans;
+    }
+    
+    public List<Integer> findElement(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        
+        int first = nums[0];
+        int second = Integer.MAX_VALUE;
+        int count1 = 1;
+        int count2 = 0;
+        
+        for (int i=1;i<nums.length;i++) {
+            if (nums[i]==first) {
+                count1++;
+            }
+            else if (nums[i]==second) {
+                count2++;
+            }
+            
+            else if (count1==0) {
+                first=nums[i];
+                count1++;
+            }
+            else if (count2==0) {
+                second=nums[i];
+                count2++;
+            }
+            
+            else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        temp.add(first);
+        temp.add(second);
+        
+        return temp;
+    }
+    
+    public int count(int nums[],int x) {
+        int count = 0;
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i]==x) 
+                count++;
+        }
+        return count;
+    }
+}
