974921739d4416ce3a1bf68521479f8d17eb1f8a
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-04-04 12:05:16 +0530

    Time: 1 ms (44.87%), Space: 40.8 MB (64.71%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        boolean visited[][] = new boolean[m][n];
+        
+        // Dp array representing no of paths from that point to finish
+        int dp[][] = new int[m][n];
+        
+        for (int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if (i==m-1 && j==n-1) {
+                    dp[i][j] = 1;
+                }
+                else {
+                    int t1 = (i+1)<m ? dp[i+1][j] : 0;
+                    int t2 = (j+1)<n ? dp[i][j+1] : 0;
+                    dp[i][j] = t1+t2;
+                }  
+            }
+        }
+        
+        return dp[0][0];
+    }
+}
