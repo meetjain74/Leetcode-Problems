0280718c488424766a734c1450194459ac546218
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-06-03 12:36:14 +0530

    Time: 1 ms (43.04%), Space: 42.4 MB (28.04%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -16,7 +16,8 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        inorderRecursive(root,ans);
+        //inorderRecursive(root,ans);
+        inorderIterative(root,ans);
         return ans;
     }
     
@@ -28,5 +29,21 @@ class Solution {
         inorderRecursive(root.left,ans);
         ans.add(root.val);
         inorderRecursive(root.right,ans);
+    }
+    
+    /*Iterative implementation using stack*/
+    private void inorderIterative(TreeNode root, List<Integer> ans) {
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode current = root;
+        while (current!=null || !s.isEmpty()) {
+            while (current!=null) {
+                s.push(current);
+                current = current.left;
+            }
+
+            current = s.pop();
+            ans.add(current.val);
+            current = current.right;
+        }
     }
 }
