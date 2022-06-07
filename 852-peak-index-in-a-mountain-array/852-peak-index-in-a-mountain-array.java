3b8f5981198d24c8c4215688fe72d2cf28e753a8
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-06-07 15:31:51 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (83.59%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -5,37 +5,15 @@ class Solution {
         
         while (low<=high) {
             int mid = low + (high-low)/2;
-            int res = isMountain(arr,mid);
-            if (res==0) {
-                return mid;
-            }
-            else if (res==1) {
+            
+            if (arr[mid]>arr[mid+1]) {
                 high=mid-1;
             }
-            else {
+            else if (arr[mid]<arr[mid+1]) {
                 low=mid+1;
             }
         }
         
-        return -1;
-    }
-    
-    // 0 means correct mountain
-    // 1 means left part of mountain is incorrect
-    // 2 means right part of mountain is incorrect
-    public int isMountain(int[] arr,int i) {
-        // Left part of mountain
-        for (int x=1;x<=i;x++) {
-            if (arr[x]<arr[x-1])
-                return 1;
-        }
-        
-        // Right part of mountain
-        for (int x=i+1;x<arr.length;x++) {
-            if (arr[x]>arr[x-1])
-                return 2;
-        }
-        
-        return 0;
+        return low;
     }
 }
