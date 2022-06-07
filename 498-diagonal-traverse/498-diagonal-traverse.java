93e256c4933a2dcb56581dce8b36e3d47aed4eeb
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-06-07 11:20:25 +0530

    Time: 4 ms (54.53%), Space: 54.6 MB (47.74%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.java
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -1,42 +1,67 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        List<List<Integer>> diagonalsList = new ArrayList<>();
-        
-        for (int i=0;i<m;i++) {
-            for (int j=0;j<n;j++) {
-                int diagNum = i+j;
-                List<Integer> diagonal;
-                if (diagonalsList.size()<=diagNum) {
-                    // diagonal list don't exist yet - Create new one
-                    diagonal = new ArrayList<Integer>();
-                    diagonalsList.add(diagonal);
-                }
-                else {
-                    diagonal = diagonalsList.get(diagNum);
-                }
-                diagonal.add(mat[i][j]);
-            }
-        }
-        
-        int k = 0;
+        int m=mat.length;
+        int n=mat[0].length;
         int res[] = new int[m*n];
         
-        // Making answer array
-        for (int i=0,size=diagonalsList.size();i<size;i++) {
-            List<Integer> diagonal = diagonalsList.get(i);
-            // Diagonal indexes which are even should be reversed
-            if (i%2==0)
-                Collections.reverse(diagonal);
+        int idx = 0; //Pointer to move on the resultant array
+        int row = 0;
+        int col = 0;
+        
+        //In each diagonal the sum of indices all through its length remains same
+        //The sum could range between 0 to (m-1)+(n-1)
+        //sum is even -> up
+        //sum is odd -> down
+        
+        for(int sum=0;sum<=m+n-2;sum++){
             
-            // Add elements to array
-            for (int j=0,len=diagonal.size();j<len;j++) {
-                res[k++]=diagonal.get(j);
+            if(sum%2==0) {
+                //Up Direction i.e row-- col++
+                while(row>=0 && col<n){
+                    res[idx++] = mat[row][col];
+                    row--;
+                    col++;
+                }
+                row++; //As row goes out of matrix and becomes negative
+                
+                //When all the columns of a row are covered i.e. col becomes n 
+                if(col==n){ 
+                    row++; //To bring it back in the matrix
+                    col--; //As col becomes n we need to decrement it
+                }
+            }
+            
+            else {
+                //Down Direction i.e row++ col--
+                while(row < m && col >= 0){
+                    res[idx++] = mat[row][col];
+                    row++;
+                    col--;
+                }
+                col++; //As col goes out of matrix and becomes negative
+                
+                //When all the rows of a column are covered i.e. row becomes m
+                if(row==m){
+                    row--; //As row becomes m we need to decrement it
+                    col++; //To bring it back in the matrix
+                }
             }
         }
         
         return res;
     }
 }
+
+//To imagine the above approach
+
+//            | (-1,0)| (-1,1)| (-1,2)| (-1,3)|
+//     _______|_______|_______|_______|_______|_______
+//     (0,-1) | (0,0) | (0,1) | (0,2) | (0,3) | (0,4)
+//     _______|_______|_______|_______|_______|_______
+//     (1,-1) | (1,0) | (1,1) | (1,2) | (1,3) | (1,4)
+//     _______|_______|_______|_______|_______|_______
+//     (2,-1) | (2,0) | (2,1) | (2,2) | (2,3) | (2,4)
+//     _______|_______|_______|_______|_______|_______
+//     (3,-1) | (3,0) | (3,1) | (3,2) | (3,3) | (3,4)
+//     _______|_______|_______|_______|_______|_______
+//            | (-1,0)| (-1,1)| (-1,2)| (-1,3)|   
