0416cdbef79ce1dfb21884688de3cdb84a26d5eb
meetjain74 <56895638+meetjain74@users.noreply.github.com>
2022-06-09 15:11:22 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (63.22%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,71 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int peakIndex = findPeakIndex(mountainArr, 0, mountainArr.length()-1);
+        
+        int result = orderAgnosticBinarySearch(mountainArr, target, 0, peakIndex);
+        
+        if (result==-1) {
+            result = orderAgnosticBinarySearch(mountainArr, target, peakIndex, 
+                                              mountainArr.length()-1);
+        }
+        
+        return result;
+    }
+    
+    private int findPeakIndex(MountainArray mountainArr, int low, int high) {
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            
+            int elemAtMid = mountainArr.get(mid);
+            int elemAtMidPlusOne = mountainArr.get(mid+1);
+            
+            if (elemAtMid>elemAtMidPlusOne) {
+                high=mid-1;
+            }
+            else if (elemAtMid<elemAtMidPlusOne) {
+                low=mid+1;
+            }
+        }
+        
+        return low;
+    }
+    
+    private int orderAgnosticBinarySearch(MountainArray mountainArr, int target, 
+                                          int low, int high) {
+        
+        boolean isAscending = mountainArr.get(high)>=mountainArr.get(low);
+        
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            int elemAtMid = mountainArr.get(mid);
+            
+            if (elemAtMid==target)
+                return mid;
+            
+            if (isAscending) {
+                if (elemAtMid>target)
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else {
+                if (elemAtMid>target)
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        
+        return -1;
+    }
+}
