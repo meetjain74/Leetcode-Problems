24216976b798ce632229f61a58917691ac94a1f4
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-13 00:08:43 +0530

    Time: 0 ms (100.00%), Space: 46.3 MB (27.19%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,14 +11,21 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> nodes = new HashSet<>();
-        ListNode curr=head;
-        while (curr!=null) {
-            if (nodes.contains(curr))
-                return true;
-            nodes.add(curr);
-            curr=curr.next;
+        if (head==null) 
+            return false;
+        
+        // Using floyd cycle detection algorithm
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (slow!=fast) {
+            if (fast==null || fast.next==null)
+                return false;
+            
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return false;
+        
+        return true;
     }
 }
