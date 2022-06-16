8b0e7e4d49a99626f43615ea7b5ac8a3b2963b8e
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-16 13:56:35 +0530

    Time: 4 ms (65.73%), Space: 127.6 MB (41.45%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        return method2(nums);
+        return method3(nums);
     }
     
     private int method1(int[] nums) {
@@ -32,5 +32,31 @@ class Solution {
         }
         
         return nums.length+1;
+    }
+    
+    // Using swap sort
+    private int method3(int[] nums) {
+        int i=0;
+        while (i<nums.length) {
+            if (nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[nums[i]-1]) {
+                swapAtIndex(nums,i,nums[i]-1);
+            }
+            else {
+                i++;
+            }
+        }
+        
+        for (int j=0;j<nums.length;j++) {
+            if (nums[j]!=j+1)
+                return j+1;
+        }
+    
+        return nums.length+1;
+    }
+    
+    private void swapAtIndex(int[] nums,int i,int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
