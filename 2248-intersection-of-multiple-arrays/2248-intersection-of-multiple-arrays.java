bac55c9dbe9e710c6f266e8221cfd9cfb000d3d9
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-16 12:06:55 +0530

    Time: 11 ms (21.36%), Space: 47.4 MB (23.85%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        int noOfArrays = nums.length;
+        int[] res;
+        if (noOfArrays==1)
+            res=nums[0];
+        else {
+            res=intersectionOfTwoArrays(nums[0],nums[1]);
+            for (int i=2;i<noOfArrays;i++) {
+                if (res.length==0)
+                    break;
+                res = intersectionOfTwoArrays(res,nums[i]);
+            }
+        }
+        
+        Arrays.sort(res);
+        List<Integer> ans = new ArrayList<>();
+        for (int i: res) {
+            ans.add(i);
+        }
+        
+        return ans;
+    }
+    
+    private int[] intersectionOfTwoArrays(int[] nums1,int[] nums2) {
+        // Stores all values inside nums2
+        HashSet<Integer> vals = new HashSet<>();
+        for (int i: nums2) {
+            vals.add(i);
+        }
+        
+        HashSet<Integer> intersections = new HashSet<>();
+        for (int i: nums1) {
+            if (vals.contains(i)) 
+                intersections.add(i);
+        }
+        
+        int[] ans = new int[intersections.size()];
+        int j=0;
+        for (Integer i: intersections) {
+            ans[j++] = i;
+        }
+        
+        return ans;
+    }
+}
