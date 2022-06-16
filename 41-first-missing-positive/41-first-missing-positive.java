47fd5a42bed1395f4d1b304c91ae5d1f7a98ca1b
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-16 13:45:28 +0530

    Time: 2 ms (99.78%), Space: 128.4 MB (25.62%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,5 +1,9 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        return method2(nums);
+    }
+    
+    private int method1(int[] nums) {
         TreeSet<Integer> vals = new TreeSet<>();
         for (int i: nums) {
             if (i>0)
@@ -8,6 +12,22 @@ class Solution {
         
         for (int i=1;i<=nums.length;i++) {
             if (!vals.contains(i))
+                return i;
+        }
+        
+        return nums.length+1;
+    }
+    
+    private int method2(int[] nums) {
+        boolean[] visited = new boolean[nums.length+1];
+        for(int i: nums) {
+           if(i>0 && i<=nums.length) {
+               visited[i] = true;
+           }
+        }
+
+        for(int i=1;i<=nums.length;i++) {
+            if(!visited[i]) 
                 return i;
         }
         
