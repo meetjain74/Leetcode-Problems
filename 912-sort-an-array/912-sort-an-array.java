b70db053d81df7dd7b2b28ebd73726deefde7a0f
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-20 01:41:45 +0530

    Time: 22 ms (40.63%), Space: 51.1 MB (95.03%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums);
+        heapSort(nums);
         return nums;
     }
     
@@ -187,5 +187,49 @@ class Solution {
         mergeSort(arr,left,mid);
         mergeSort(arr,mid+1,right);
         merge(arr,left,right,mid);
+    }
+    
+    /******************************HEAP SORT******************************/
+    
+    public void heapSort(int arr[]) {
+        int n=arr.length; 
+        buildMaxHeap(arr);
+        
+        // One by one extract an element from heap 
+        for (int i=n-1;i>=1;i--) { 
+            // Move current root to end 
+            int temp=arr[0]; 
+            arr[0]=arr[i]; 
+            arr[i]=temp; 
+            // call max heapify on the reduced heap 
+            heapify(arr,i,0); 
+        } 
+    }
+
+    /*Time Complexity-O(n)*/
+    public void buildMaxHeap(int arr[]) {
+        int n=arr.length; 
+        for (int i=n/2-1;i>=0; i--) 
+            heapify(arr,n,i);
+    }
+
+    /*Time Complexity-O(nlogn)*/
+    public void heapify(int arr[],int n,int i) { 
+        int largest=i;  // Initialize largest as root 
+        int l=2*i+1;
+        int r=2*i+2;
+
+        if (l<n && arr[l]>arr[largest]) 
+            largest=l; 
+        if (r<n && arr[r]>arr[largest]) 
+            largest=r; 
+
+        if (largest!=i) 
+        { 
+            int swap=arr[i]; 
+            arr[i]=arr[largest]; 
+            arr[largest]=swap; 
+            heapify(arr,n,largest); 
+        } 
     }
 }
