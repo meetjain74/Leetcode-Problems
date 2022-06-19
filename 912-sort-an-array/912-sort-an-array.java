1bdb6e13d1ecc7a300235b89d08dffe5615f972b
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-20 01:27:42 +0530

    Time: 2451 ms (5.02%), Space: 55.1 MB (81.75%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        insertionSort(nums);
+        quickSort(nums);
         return nums;
     }
     
@@ -8,13 +8,10 @@ class Solution {
     
     public void bubbleSort(int array[]) {
         int temp,flag;
-        for (int i=0;i<array.length-1;i++)
-        {
+        for (int i=0;i<array.length-1;i++) {
             flag=0;
-            for (int j=0;j<array.length-i-1;j++)
-            {
-                if (array[j]>array[j+1])
-                {
+            for (int j=0;j<array.length-i-1;j++) {
+                if (array[j]>array[j+1]) {
                     flag=1;
                     temp=array[j];
                     array[j]=array[j+1];
@@ -28,16 +25,12 @@ class Solution {
     
     /******************************SELECTION SORT**********************************/
     
-    public void selectionSort(int array[])
-    {
+    public void selectionSort(int array[]) {
         int small,temp;
-        for (int i=0;i<array.length-1;i++)
-        {
+        for (int i=0;i<array.length-1;i++) {
             small=array[i];
-            for (int j=i;j<array.length;j++)
-            {
-                if (array[j]<small)
-                {
+            for (int j=i;j<array.length;j++) {
+                if (array[j]<small) {
                     temp=array[j];
                     array[j]=small;
                     small=temp;
@@ -49,19 +42,47 @@ class Solution {
     
     /******************************INSERTION SORT**********************************/
     
-    public void insertionSort(int array[])
-    {
+    public void insertionSort(int array[]) {
         int temp,j;
-        for (int i=1;i<array.length;i++)
-        {
+        for (int i=1;i<array.length;i++) {
             temp=array[i];
             j=i-1;
-            while (j>=0 && array[j]>temp)
-            {
+            while (j>=0 && array[j]>temp) {
                 array[j+1]=array[j];
                 j--;
             }
             array[j+1]=temp;
         }
+    }
+    
+    /******************************QUICK SORT**********************************/
+    
+    public void quickSort(int array[]) {
+        quickSort(array,0,array.length-1);
+    }
+
+    public void quickSort(int array[],int p,int r) {
+        if (p<r) {
+            int q=partition(array,p,r);
+            quickSort(array,p,q-1);
+            quickSort(array,q+1,r);	
+        }
+    }
+
+    public void swapAtIndex(int array[],int x,int y) {
+        int temp=array[x];
+        array[x]=array[y];
+        array[y]=temp;
+    }
+
+    public int partition(int array[],int p,int r) {
+        int pivot=array[r];
+        int i=p-1;
+        for (int j=p;j<=r-1;j++) {
+            if (array[j]<=pivot)
+                swapAtIndex(array,++i,j);
+        }
+        swapAtIndex(array,++i,r);
+        return i;
     }
 }
