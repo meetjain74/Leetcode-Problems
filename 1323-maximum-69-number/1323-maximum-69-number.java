503c5ee0bd72c084e4c48bfd02d203b3600d58a8
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-20 02:30:52 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (25.00%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,5 +1,9 @@
 class Solution {
     public int maximum69Number (int num) {
+        return method2(num);
+    }
+    
+    public int method1(int num) {
         int firstSix = -1;
         
         String n = Integer.toString(num);
@@ -13,5 +17,17 @@ class Solution {
             n = n.substring(0,firstSix)+"9"+n.substring(firstSix+1);
         
         return Integer.parseInt(n);
+    }
+    
+    public int method2(int num) {
+        int f  = num/1000;
+        int s  = num/100%10;
+        int t  = num/10%10;
+        int foo  = num%10;
+        if(f==6) num+=3000;
+        else if(s==6) num+=300;
+        else if(t==6) num+=30;
+        else if(foo==6) num+=3;
+        return num;
     }
 }
