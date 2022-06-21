5f2e7263ebfa1cfbd8ed073bccb0be63ee4b3edf
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-21 17:20:45 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (47.95%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -17,7 +17,8 @@ class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         //preorderRecursive(root,ans);
-        preorderIterative(root,ans);
+        //preorderIterative(root,ans);
+        preorderMorrisTraversal(root,ans);
         return ans;
     }
     
@@ -44,6 +45,40 @@ class Solution {
 
             current = s.pop();
             current = current.right;
+        }
+    }
+    
+    /*Morris traversal*/
+    private void preorderMorrisTraversal(TreeNode root, List<Integer> ans) {
+        TreeNode current = root;
+        
+        while (current!=null) {
+            // If current does not have a left print current and move to right
+            if (current.left==null) {
+                ans.add(current.val);
+                current = current.right;
+            }
+            
+            // Else before moving to left make a threaded connection
+            else {
+                // Get rightmost node in left subtree
+                TreeNode prev = current.left;
+                while (prev.right!=null && prev.right!=current) {
+                    prev = prev.right;
+                }
+                
+                if (prev.right==null) {
+                    // Make threaded connection
+                    prev.right = current;
+                    ans.add(current.val);
+                    current = current.left;
+                }
+                else {
+                    // Remove threaded connection - Restore tree
+                    prev.right = null;
+                    current = current.right;
+                }
+            }
         }
     }
 }
