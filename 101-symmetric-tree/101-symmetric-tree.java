f9638e5530ccdf5893130c4f27735191bb54eaa9
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-21 16:56:48 +0530

    Time: 3 ms (11.32%), Space: 43.1 MB (7.23%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,6 +15,11 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        return isSymmetricIterative(root);
+    }
+    
+    // Recursive solution
+    private boolean isSymmetricRecursive(TreeNode root) {
         if (root==null)
             return true;
         
@@ -33,5 +38,41 @@ class Solution {
         }
         
         return false;
+    }
+    
+    // Iterative solution
+    private boolean isSymmetricIterative(TreeNode root) {
+        if (root==null)
+            return true;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            TreeNode x = queue.poll(); //Of left subtree
+            TreeNode y = queue.poll(); // Of right subtree
+            
+            if(x==null && y==null) {
+                continue;
+            }
+
+            if(x==null || y==null) {
+                return false;
+            }
+
+            if(x.val!=y.val) {
+                return false;
+            }
+            
+            queue.add(x.left);
+            queue.add(y.right);
+            
+            queue.add(x.right);
+            queue.add(y.left);
+        }
+        
+        return true;
     }
 }
