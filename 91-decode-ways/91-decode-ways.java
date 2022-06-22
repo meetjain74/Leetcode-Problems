e4c60d471fb8a1394d96f5d981d6bcde028f7c00
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-22 10:53:27 +0530

    Time: 2 ms (66.34%), Space: 42.2 MB (59.06%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -2,33 +2,24 @@ class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         
-        // dp[i] represents no of encodings in string s for index [0,i]
-        int dp[] = new int[n];
+        if (s.charAt(0)=='0')
+            return 0;
         
-        for (int i=0;i<n;i++) {
-            if (i==0) {
-                int x = s.charAt(i)-'0';
-                dp[i] = (x==0) ? 0 : 1;
+        // dp[i] represents no of encodings upto length i
+        int dp[] = new int[n+1];
+        dp[0] = 1; // 1 encoding upto no length
+        dp[1] = 1; // 1 encoding upto 1 length as now that num>0
+        
+        for (int i=2;i<=n;i++) {
+            if (s.charAt(i-1)>'0'){
+                dp[i] = dp[i-1];
             }
-            else {
-                int x = s.charAt(i)-'0';
-                int y = s.charAt(i-1)-'0';
-                int num = y*10+x;
-                
-                if (y==0 || num>26) {
-                    // Encodings upto previous index and current letter at index i
-                    dp[i] = dp[i-1] * ((x==0) ? 0 : 1);
-                }
-                else {
-                    if (i==1)
-                        dp[i] = dp[i-1] * ((x==0) ? 0 : 1) + ((num==0) ? 0 : 1);
-                    else
-                        dp[i] = dp[i-1] * ((x==0) ? 0 : 1) + 
-                                dp[i-2] * ((num==0) ? 0 : 1);
-                }
+            
+            if(s.charAt(i-2)=='1' || s.charAt(i-2)=='2' && s.charAt(i-1)<'7') {
+                dp[i] += dp[i-2];
             }
         }
         
-        return dp[n-1];
+        return dp[n];
     }
 }
