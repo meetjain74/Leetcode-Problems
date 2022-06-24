9c17f07710f9dadf10f0777294244bb15df83e15
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-24 23:31:19 +0530

    Time: 2 ms (76.60%), Space: 45.9 MB (63.06%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.java
@@ -15,23 +15,28 @@
  */
 class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return generateTrees(1,n);
+        List<TreeNode>[][] memo= new ArrayList[n+1][n+1];
+        return generateTrees(memo,1,n);
     }
     
-    private List<TreeNode> generateTrees(int start,int end) {
+    private List<TreeNode> generateTrees(List<TreeNode>[][] memo,int start,int end) {
         List<TreeNode> list = new ArrayList<>();
         if (start>end) {
             list.add(null);
             return list;
         }
         
+        // If list already contains tree
+        if (memo[start][end]!=null)
+            return memo[start][end];
+        
         // Considering each node from start to end as root once
         for (int i=start;i<=end;i++) {
             // Root will have value i
             // Root left will have value from start to i-1
             // Root right will have values from i+1 to end
-            List<TreeNode> left = generateTrees(start,i-1);
-            List<TreeNode> right = generateTrees(i+1,end);
+            List<TreeNode> left = generateTrees(memo,start,i-1);
+            List<TreeNode> right = generateTrees(memo,i+1,end);
             for (TreeNode l: left) {
                 for (TreeNode r: right) {
                     TreeNode curr = new TreeNode(i);
@@ -43,6 +48,7 @@ class Solution {
             
         }
         
+        memo[start][end]=list;
         return list;
     }
 }
