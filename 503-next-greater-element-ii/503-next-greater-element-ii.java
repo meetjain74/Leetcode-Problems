4857a01d7f7fcd6dc91515215c7df2dce6d4b7dd
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-28 01:40:15 +0530

    Time: 43 ms (48.57%), Space: 55.3 MB (20.95%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -1,5 +1,9 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        return method2(nums);
+    }
+    
+    public int[] method1(int[] nums) {
         int n = nums.length;
         int[] nge = new int[n];
         Arrays.fill(nge,-1);
@@ -10,6 +14,31 @@ class Solution {
                     break;
                 }
             }
+        }
+        
+        return nge;
+    }
+    
+    // Optimized using stack
+    public int[] method2(int[] nums) {
+        int n = nums.length;
+        int[] nge = new int[n];
+        Stack<Integer> s = new Stack<>();
+        
+        // Think of circular array as arrays written twice
+        for (int i=2*n-1;i>=0;i--) {
+            while (!s.isEmpty() && s.peek()<=nums[i%n]) {
+                s.pop();
+            }
+            
+            if (i<n) {
+                if (s.isEmpty())
+                    nge[i]=-1;
+                else
+                    nge[i]=s.peek();
+            }
+            
+            s.push(nums[i%n]);
         }
         
         return nge;
