c8e3c329cb76e803f366cc83ada15821a08fda2c
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-28 01:12:43 +0530

    Time: 8 ms (23.10%), Space: 44.9 MB (7.74%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -5,6 +5,9 @@ class Solution {
         int[] nge = new int[nums2.length];
         Stack<Integer> s = new Stack<>();
         
+        // Element to nge for nums2
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
         for (int i=nums2.length-1;i>=0;i--) {
             while (!s.isEmpty() && s.peek()<=nums2[i]) {
                 s.pop();
@@ -18,15 +21,10 @@ class Solution {
             }
             
             s.push(nums2[i]);
+            map.put(nums2[i],nge[i]);
         }
         
         int ans[] = new int[nums1.length];
-        
-        // Element to nge for nums2
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for (int i=0;i<nums2.length;i++) {
-            map.put(nums2[i],nge[i]);
-        }
         
         for (int i=0;i<nums1.length;i++) {
             ans[i]=map.get(nums1[i]);
