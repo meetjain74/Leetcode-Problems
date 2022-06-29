0a047022246899b6bc61c470a43d284dc9020c58
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-29 11:13:06 +0530

    Time: 7 ms (47.52%), Space: 50.7 MB (70.09%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -13,27 +13,61 @@
  *     }
  * }
  */
-class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        // Values of Tree
-        HashMap<Integer,Boolean> values = new HashMap<>();
-        return inorder(values,root,k);
+
+class BSTIterator {
+    Stack<TreeNode> stack;
+    boolean reverse = true; // Ascending order or descending order
+
+    public BSTIterator(TreeNode root,boolean reverse) {
+        this.reverse=reverse; 
+        stack = new Stack<>();
+        while (root!=null) {
+            stack.push(root);
+            if (reverse) // Descending
+                root=root.right;
+            else
+                root=root.left;
+        }
     }
     
-    private boolean inorder(HashMap<Integer,Boolean> map,TreeNode root,int k) {
+    public int next() {
+        TreeNode curr = stack.pop();
+        TreeNode temp = reverse ? curr.left : curr.right;
+        while(temp!=null) {
+            stack.push(temp);
+            if (reverse) // Descending
+                temp=temp.right;
+            else
+                temp=temp.left;
+        }
+        return curr.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
         if (root==null)
             return false;
         
-        if (inorder(map,root.left,k))
-            return true;
+        // Two pointer approach
+        BSTIterator left = new BSTIterator(root,false);
+        BSTIterator right = new BSTIterator(root,true);
         
-        if (map.containsKey(k-root.val)) {
-            return true;
+        int first = left.next();
+        int second = right.next();
+        
+        while (first<second) {
+            if (first+second==k)
+                return true;
+            else if (first+second>k)
+                second=right.next();
+            else // first+second<k
+                first=left.next();
         }
-        map.put(root.val,true);
-        
-        if (inorder(map,root.right,k))
-            return true;
         
         return false;
     }
