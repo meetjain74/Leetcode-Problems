579df35caa9a9c7e762a55ce2780b898787c636d
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-06-30 10:44:39 +0530

    Time: 4 ms (90.02%), Space: 43.5 MB (82.61%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,27 +3,14 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         
-        int mid1 = (n-1)/2;
-        int mid2 = n/2;
+        int mid = n/2;
         
-        int moves1 = 0; // Move complete array to equal to mid1
-        int moves2 = 0; // Move complete array to equal to mid2
+        int moves = 0; // Move complete array to equal to mid
         
         for (int i=0;i<n;i++) {
-            if (i<mid1) {
-                moves1 += (nums[mid1]-nums[i]);
-                moves2 += (nums[mid2]-nums[i]);
-            }
-            else if (i<mid2) {
-                moves1 += (nums[i]-nums[mid1]);
-                moves2 += (nums[mid2]-nums[i]);
-            }
-            else {
-                moves1 += (nums[i]-nums[mid1]);
-                moves2 += (nums[i]-nums[mid2]);
-            }
+            moves += Math.abs(nums[mid]-nums[i]);
         }
         
-        return Math.min(moves1,moves2);
+        return moves;
     }
 }
