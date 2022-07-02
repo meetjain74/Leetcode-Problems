fa24fd45a4b33d800f33eaf01820a355e46fcc15
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-02 19:02:24 +0530

    Time: 7 ms (76.26%), Space: 64.5 MB (59.51%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,44 +1,83 @@
+class DisjointSet
+{
+	int n;
+	int parent[];
+	int rank[];
+
+	DisjointSet(int n) {
+		this.n=n;
+		parent=new int[n];
+		rank=new int[n];
+		makeSet();
+	}
+
+	public void makeSet(int x) {
+		if (x>=n)
+			throw new IllegalArgumentException("makeSet takes parameter"+ 
+                                               " value which is not available");
+		parent[x]=-1; // -1 represents elements are in their own set
+		rank[x]=0;
+	}
+
+	private void makeSet() {
+		for (int i=0;i<n;i++)
+			makeSet(i);
+	}
+
+	// Find absolute parent of x
+	public int find(int x) {
+		if (parent[x]==-1)
+			return x;
+		return parent[x]=find(parent[x]);
+	}
+
+	// Union to make absolute parent of both same
+	public void union(int x,int y) {
+		int xParent=find(x);
+		int yParent=find(y);
+		int xRank=rank[xParent];
+		int yRank=rank[yParent];
+
+		// If elements are in same set - no need for union
+		if (xParent==yParent)
+			return;
+
+		// If x's rank is greater than y's rank
+		if (xRank>yRank)
+			parent[yParent]=xParent;
+
+		// Else If y's rank is greater than x's rank 
+		else if (yRank>xRank)
+			parent[xParent]=yParent;
+
+		// If both ranks are equal
+		else {
+			// Move y under x 
+			parent[yParent]=xParent;
+			// Increment rank by 1 for resultant tree
+			rank[xParent]++;
+		}
+	}
+}
+
+
 class Solution {
     public int makeConnected(int n, int[][] connections) {
         // For n computers to form a network, require n-1 connections
         if (connections.length < n-1)
             return -1; // Enough connections not available
         
-        List<List<Integer>> graph = new ArrayList<>(); // Adjacency list
-        buildGraph(graph,connections,n);
+        DisjointSet ds = new DisjointSet(n);
+        for (int[] c:connections) {
+            ds.union(c[0],c[1]);
+        }
         
-        boolean[] visited = new boolean[n];
-        
-        int components = 0; // Find no of components in graph
+        int components = 0;
         for (int i=0;i<n;i++) {
-            if (!visited[i]) {
+            if (ds.parent[i]==-1)
                 components++;
-                dfs(graph,visited,i);
-            }
         }
         
-        // no of connections required to make network connected
         return components-1;
-    }
-    
-    private void buildGraph(List<List<Integer>> graph,int[][] connections,int n) {
-        for (int i=0;i<n;i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-        
-        for (int[] c: connections) {
-            graph.get(c[0]).add(c[1]);
-            graph.get(c[1]).add(c[0]);
-        }
-    }
-    
-    private void dfs(List<List<Integer>> graph, boolean[] visited,int node) {
-        if (visited[node])
-            return;
-        
-        visited[node] = true;
-        for (Integer neighbor: graph.get(node)) {
-            dfs(graph,visited,neighbor);
-        }
     }
 }
