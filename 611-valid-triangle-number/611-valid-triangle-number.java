1e61df9116a688d46d8389d49c9048e9342ff026
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-04 13:38:59 +0530

    Time: 38 ms (78.45%), Space: 44.4 MB (19.20%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.java
+++ 611-valid-triangle-number/611-valid-triangle-number.java
@@ -2,26 +2,31 @@ class Solution {
     public int triangleNumber(int[] nums) {
         Arrays.sort(nums);
         int count = 0;
-        for (int i=0;i<nums.length-2;i++) {
-            int a = nums[i]; // First side of triangle
+        
+        for (int k=nums.length-1;k>=2;k--) {
+            int c = nums[k]; // Third side of triangle
             
-            for (int j=i+1;j<nums.length-1;j++) {
-                int b = nums[j]; // Second side of triangle 
+            int start = 0;
+            int end = k-1;
+            
+            while (start<end) {
+                int a = nums[start]; // First side of traingle
+                int b = nums[end]; // Second side of triangle
                 
-                for (int k=j+1;k<nums.length;k++) {
-                    int c = nums[k]; // Third side of triangle
-                    
-                    // As a,b,c is sorted just need to check if a+b>c
-                    if (a+b>c) {
-                        count++;
-                    }
-                    else {
-                        break;
-                    }
+                // As a,b,c is sorted just need to check if a+b>c
+                if (a+b>c) {
+                    // At current start this holds true
+                    // Means true for all between start and end as value of a increases
+                    count += (end-start);
+                    end--;
+                }
+                else {
+                    // Increment start to increase the sum of a+b
+                    start++;
                 }
             }
         }
-        
+
         return count;
     }
 }
