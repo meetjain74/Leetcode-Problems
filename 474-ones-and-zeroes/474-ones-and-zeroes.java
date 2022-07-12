de146dccc72931404107c00b0bd83acc0699bff4
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-12 19:03:01 +0530

    Time: 23 ms (91.79%), Space: 41.5 MB (97.65%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,5 +1,9 @@
 class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
+        return bottomUpDP(strs,m,n);
+    }
+    
+    private int memoTopDown(String[] strs,int m,int n) {
         int dp[][][] = new int[strs.length][m+1][n+1];
         for (int i=0;i<strs.length;i++) {
             for (int j=0;j<=m;j++) {
@@ -39,5 +43,34 @@ class Solution {
         int h2 = 1 + helper(strs,index+1,m-currM,n-currN,dp);
         
         return dp[index][m][n]=Math.max(h1,h2);
+    }
+    
+    private int bottomUpDP(String[] strs,int m,int n) {
+        // dp[i][j] represents length of largest subset
+        // containing at most i 0's and j 1's
+        int dp[][] = new int[m+1][n+1];
+        
+        // Consider one string at a time and update the dp
+        for (String str: strs) {
+            int zeroes = countZeroes(str);
+            int ones = str.length()-zeroes;
+            
+            for (int i=m;i>=zeroes;i--) {
+                for (int j=n;j>=ones;j--) {
+                    dp[i][j] = Math.max(dp[i][j],1+dp[i-zeroes][j-ones]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
+    private int countZeroes(String str) {
+        int count = 0;
+        for (char ch: str.toCharArray()) {
+            if (ch=='0')
+                count++;
+        }
+        return count;
     }
 }
