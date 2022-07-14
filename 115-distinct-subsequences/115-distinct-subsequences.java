a027fe5530a634f858f19523f191e9ec9fb9fc73
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-15 01:16:27 +0530

    Time: 28 ms (56.51%), Space: 50.7 MB (11.78%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -6,27 +6,23 @@ class Solution {
         // dp[i][j] stores the count of distinct subsequences of 
         // string1 upto length i which equals string2 upto length j
         int dp[][] = new int [m+1][n+1];
+        
+        // dp[i][0] is 1 as string2 is empty
         for (int i=0;i<=m;i++) {
-            Arrays.fill(dp[i],-1);
+            dp[i][0]=1;
         }
-        return count(s,t,1,1,m,n,dp);
-    }
-    
-    private int count(String s,String t,int slen,int tlen,int m,int n,int[][] dp) {
-        if (tlen>n)
-            return 1;
-        if (slen>m)
-            return 0;
         
-        if (dp[slen][tlen]!=-1)
-            return dp[slen][tlen];
+        // dp[0][j] is 0 as string1 is empty
         
-        if (s.charAt(slen-1)==t.charAt(tlen-1)) {
-            return dp[slen][tlen]=count(s,t,slen+1,tlen+1,m,n,dp)+
-                                    count(s,t,slen+1,tlen,m,n,dp);
+        for (int i=1;i<=m;i++) {
+            for (int j=1;j<=n;j++) {
+                if (s.charAt(i-1)==t.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
         }
-        else {
-            return dp[slen][tlen]=count(s,t,slen+1,tlen,m,n,dp);
-        }
+        
+        return dp[m][n];
     }
 }
