0476eca882b5821b2260275ab8f51b0039793ac6
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-15 22:09:22 +0530

    Time: 20 ms (30.05%), Space: 44.2 MB (26.98%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.java
+++ 43-multiply-strings/43-multiply-strings.java
@@ -1,93 +1,43 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int n1 = num1.length();
-        int n2 = num2.length();
+        int m = num1.length();
+		int n = num2.length();
+
+		// Base Conditions
+		if (m == 0 || n == 0 || "0".equals(num1) || "0".equals(num2)) {
+			return "0";
+		}
+		if ("1".equals(num1)) {
+			return num2;
+		}
+		if ("1".equals(num2)) {
+			return num1;
+		}
         
-        if (n1<n2)
-            return multiply(num2,num1);
+        // Result can be maximum of length M + N.
+		// For example 99 * 999 = 98901 (Result is of length 5)
+		int[] result = new int[m + n];
         
-        // Now n1>=n2
+        for (int i = m - 1; i >= 0; i--) {
+			for (int j = n - 1; j >= 0; j--) {
+				int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+				
+                // Adding previous values in result array into this product.
+				product += result[i + j + 1];
+
+				// Adding the new product into the result array
+				result[i + j + 1] = product % 10;
+				result[i + j] += product / 10;
+			}
+		}
         
         String res = "";
-        
-        for (int i=0;i<n2;i++) {
-            // Take one character of num2 at a time
-            // and multply it by num1
-            
-            int x = num2.charAt(i)-'0';
-            String temp = "";
-            int carry = 0;
-            for (int j=n1-1;j>=0;j--) {
-                int y = num1.charAt(j)-'0';
-                int z = x*y+carry;
-                carry = z/10;
-                z = z%10;
-                temp = z+temp;
-            }
-            if (carry!=0)
-                temp = carry+temp;
-            
-            if (res.equals(""))
-                res=temp;
-            else 
-                res = addTwoStrings(res+"0",temp);
+        for (int r: result) {
+            if (res.length()==0 && r==0)
+                continue;
+            res = res+r;
         }
-        
-        res=removeFrontZeroes(res);
-        return res;
-    }
-    
-    
-    private String addTwoStrings(String num1, String num2) {
-        String res = "";
-        int i=num1.length()-1;
-        int j=num2.length()-1;
-        
-        int count=0;
-        
-        while (i>=0 && j>=0) {
-            int x = num1.charAt(i)-'0';
-            int y = num2.charAt(j)-'0';
-            
-            int z = x+y+count;
-            count = z/10;
-            z = z%10;
-            
-            res = z+res;
-            i--;
-            j--;
-        }
-        
-        while (i>=0) {
-            int x = num1.charAt(i)-'0';
-            int z = x+count;
-            count = z/10;
-            z = z%10;
-            res = z+res;
-            i--;
-        }
-        
-        while (j>=0) {
-            int y = num2.charAt(j)-'0';
-            int z = y+count;
-            count = z/10;
-            z = z%10;
-            res = z+res;
-            j--;
-        }
-        
-        if (count!=0)
-            res = count+res;
         
         return res;
-    }
-    
-    private String removeFrontZeroes(String s) {
-        int i;
-        for (i=0;i<s.length()-1;i++) {
-            if (s.charAt(i)!='0')
-                break;
-        }
-        return s.substring(i);
     }
 }
