27ef6f58a799b99a63042f7cc15e73c930b1d469
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-17 00:46:16 +0530

    Time: 91 ms (32.03%), Space: 44.4 MB (46.90%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -5,16 +5,41 @@ class Solution {
         // dp[i] represents length of longest increasing subsequence
         // that ends at index i (index i is included in lis at dp[i])
         int dp[] = new int[n];
-        Arrays.fill(dp,1); // 1 length subsequence
+        Arrays.fill(dp,1); // 1 length subsequence 
+        
+        int prev[] = new int[n]; // Store the prev index for LIS 
+        int lastIndex = 0; // Last index for LIS
         
         int lis = 1;
         for (int i=1;i<n;i++) {
-            for (int prev=0;prev<i;prev++) {
-                if (nums[i]>nums[prev])
-                    dp[i] = Math.max(dp[i],1+dp[prev]);
+            prev[i]=i;
+            for (int prv=0;prv<i;prv++) {
+                if (nums[i]>nums[prv]) {
+                    if (dp[i]<1+dp[prv]) {
+                        dp[i] = 1+dp[prv];
+                        prev[i] = prv;
+                    }
+                }
+                
             }
-            lis = Math.max(lis,dp[i]); // Update Max length increasing subsequence   
+            // Update Max length increasing subsequence   
+            if (dp[i]>lis) {
+                lis = dp[i];
+                lastIndex=i;
+            }
         }
+        
+        // Print LIS
+        int LIS[] = new int[lis];
+        LIS[lis-1] = nums[lastIndex];
+        int index=lis-2;
+        while (prev[lastIndex]!=lastIndex) {
+            lastIndex=prev[lastIndex];
+            LIS[index]=nums[lastIndex];
+            index--;
+        }
+        
+        //System.out.println(Arrays.toString(LIS));
         
         return lis;
     }
