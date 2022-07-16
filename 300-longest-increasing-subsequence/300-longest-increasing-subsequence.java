5663113b4256be048dbb33f74304e8a5cce4650f
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-17 01:19:07 +0530

    Time: 4 ms (93.35%), Space: 44.6 MB (40.99%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,46 +1,37 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
+        int lis[] = new int[n]; // Need not necessarily store lis
+        int len = 0;
         
-        // dp[i] represents length of longest increasing subsequence
-        // that ends at index i (index i is included in lis at dp[i])
-        int dp[] = new int[n];
-        Arrays.fill(dp,1); // 1 length subsequence 
-        
-        int prev[] = new int[n]; // Store the prev index for LIS 
-        int lastIndex = 0; // Last index for LIS
-        
-        int lis = 1;
-        for (int i=1;i<n;i++) {
-            prev[i]=i;
-            for (int prv=0;prv<i;prv++) {
-                if (nums[i]>nums[prv]) {
-                    if (dp[i]<1+dp[prv]) {
-                        dp[i] = 1+dp[prv];
-                        prev[i] = prv;
-                    }
-                }
-                
-            }
-            // Update Max length increasing subsequence   
-            if (dp[i]>lis) {
-                lis = dp[i];
-                lastIndex=i;
-            }
+        for (int num: nums) {
+            
+            // Find correct position to replace/add num in lis
+            // Using binary search
+            int x = getPos(lis,num,len);
+            
+            // Low corresponds to that index 
+            lis[x] = num;
+            if (x==len)
+                len++;
         }
         
-        // Print LIS
-        int LIS[] = new int[lis];
-        LIS[lis-1] = nums[lastIndex];
-        int index=lis-2;
-        while (prev[lastIndex]!=lastIndex) {
-            lastIndex=prev[lastIndex];
-            LIS[index]=nums[lastIndex];
-            index--;
+        return len;
+    }
+
+    private int getPos(int[] lis,int num,int len) {
+        int low = 0;
+        int high = len-1;
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            if (lis[mid]==num)
+                return mid;
+            else if (lis[mid]>num)
+                high=mid-1;
+            else
+                low=mid+1;
         }
-        
-        //System.out.println(Arrays.toString(LIS));
-        
-        return lis;
+        return low;
+        // low corresponds to first index whose value greater than or equal to num
     }
 }
