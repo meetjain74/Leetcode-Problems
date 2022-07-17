f5b9678e45ac966e356cb12931992c2b4f178c32
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-17 10:08:18 +0530

    Time: 9 ms (51.12%), Space: 58.8 MB (27.02%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -3,25 +3,28 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        // dp[i][j] stores max side of square possible at i,j in the matrix
-        // of first i rows and j columns
-        int dp[][] = new int[m][n];
+        // dp[j] stores max side of square possible at j in the matrix
+        // of first j columns
+        int dp[] = new int[n];
+        int prev[] = new int[n];
         
         int maxSide = 0;
     
         for (int i=0;i<m;i++) {
+            dp=new int[n];
             for (int j=0;j<n;j++) {
                 if (i==0 || j==0) {
-                    dp[i][j] = matrix[i][j]=='1' ? 1:0;
+                    dp[j] = matrix[i][j]=='1' ? 1:0;
                 }
                 else {
                     if (matrix[i][j]=='0')
-                        dp[i][j]=0;
+                        dp[j]=0;
                     else
-                        dp[i][j]=1+min(dp[i-1][j],dp[i-1][j-1],dp[i][j-1]);
-                }
-                maxSide=Math.max(maxSide,dp[i][j]);
+                        dp[j]=1+min(prev[j],prev[j-1],dp[j-1]);
+                } 
+                 maxSide=Math.max(maxSide,dp[j]);
             }
+            prev=dp;
         }
         
         return maxSide*maxSide;
