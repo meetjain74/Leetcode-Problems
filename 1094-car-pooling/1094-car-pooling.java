2f09b724f1afb092e7e1cb355d5fc6cf832d6fc7
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-28 01:57:34 +0530

    Time: 1 ms (100.00%), Space: 42 MB (90.54%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        
+        return method2(trips,capacity);
+    }
+    
+    // Sort + PriorityQueue
+    public boolean method1(int[][] trips, int capacity) {
         // Sort trips in ascending order of "from" location
         Arrays.sort(trips,new Comparator<>() {
             public int compare(int[] t1,int[] t2) {
@@ -23,6 +27,24 @@ class Solution {
             }
             pq.add(new int[]{trip[2],trip[0]});
             current += trip[0];
+            if (current>capacity)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    // O(N) Time and space
+    public boolean method2(int[][] trips, int capacity) {
+        int loc[] = new int[1001]; // No of passengers at each location in bus
+        for (int[] trip: trips) {
+            loc[trip[1]] += trip[0];
+            loc[trip[2]] -= trip[0];
+        }
+        
+        int current = 0; // Current passengers
+        for (int i=0;i<1001;i++) {
+            current += loc[i];
             if (current>capacity)
                 return false;
         }
