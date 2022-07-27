e8ca58ff589d447765723b8854d0b88cbfdc336e
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-27 13:16:48 +0530

    Time: 4 ms (47.23%), Space: 68.6 MB (23.97%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        boolean dp[] = new boolean[n];
-        dp[n-1]=true;
+        
+        // Determines the last index through which we can reach 
+        // last index of array
+        int lastGoodIndex = n-1; 
         for (int i=n-2;i>=0;i--) {
             int curr = nums[i];
-            for (int j=1;j<=curr;j++) {
-                if (i+j<n && dp[i+j]) {
-                    dp[i]=true;
-                    break;
-                }
+            if (i+nums[i]>=lastGoodIndex) {
+                lastGoodIndex=i;
             }
         }
-        return dp[0];
+        
+        return lastGoodIndex==0;
     }
 }
