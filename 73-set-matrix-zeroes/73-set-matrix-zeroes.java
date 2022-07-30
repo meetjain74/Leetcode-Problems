1ab88024c1af20f226dc12935d75627522d1c991
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-07-30 22:34:30 +0530

    Time: 1 ms (97.32%), Space: 54 MB (64.54%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // Determine which rows and columns to update
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+        
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (matrix[i][j]==0) {
+                    rows[i]=true;
+                    cols[j]=true;
+                }
+            }
+        }
+        
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
