903137efd8fefb15dbce4d45c103f21b2c567bc2
Meet Jain <56895638+meetjain74@users.noreply.github.com>
2022-08-04 00:37:14 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (71.43%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.java
+++ 365-water-and-jug-problem/365-water-and-jug-problem.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean canMeasureWater(int jug1, int jug2, int target) {
-        if (jug1+jug2==target)
-            return true;
         if (jug1+jug2<target)
             return false;
         
-        // BFS
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        HashSet<Integer> visited = new HashSet<>();
-        int[] steps = {jug1,jug2,-jug1,-jug2};
-        
-        while (!q.isEmpty()) {
-            int curr = q.poll();
-            for (int step: steps) {
-                int total = curr+step;
-                if (total==target)
-                    return true;
-                if (total<0 || total>jug1+jug2 || visited.contains(total))
-                    continue;
-                
-                visited.add(total);
-                q.add(total);
-            } 
-        }
-        
-        return false;
+        // If target is multiple of gcd of both capacities
+        // Then possible
+        return target % gcd(jug1,jug2) == 0;
+    }
+    
+    private int gcd(int a,int b) {
+        if (b==0)
+            return a;
+        return gcd(b,a%b);
     }
 }
